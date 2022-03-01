3b6f11b1e1fc35ece372a1d453abbc095bef954c
frankemmy98 <87398729+frankemmy98@users.noreply.github.com>
2022-03-01 06:37:15 -0800

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../index.scss'
+
+export default function Navbar () {
+    return <nav>
+        <img src="../logo192.png" alt="navbar-logo" />
+        <h1>ReactFacts</h1>
+        <h2>React Course - Project 1</h2>
+    </nav>
+}
+
+
